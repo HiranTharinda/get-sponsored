8574e7ebe4007a8edad7b0cf491481d15187d612
hiran <hiran@surge.global>
2023-07-22 12:24:37 +0100

    gtag issue fix

--- src/main.js
+++ src/main.js
@@ -7,9 +7,15 @@ import axios from "axios";
 import { createHead } from '@vueuse/head';
 import VueGtag from "vue-gtag";
 
-const gtag = (VueGtag, { config: { id: process.env.VUE_APP_G_TAG } })
 const head = createHead()
 axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
 
-
-createApp(App).use(router).use(head).use(gtag).use(store).use(Vue3Lottie).mount('#app')
+createApp(App)
+    .use(VueGtag, {
+        config: { id: process.env.VUE_APP_G_TAG }
+    })
+    .use(router)
+    .use(head)
+    .use(store)
+    .use(Vue3Lottie)
+    .mount('#app')
